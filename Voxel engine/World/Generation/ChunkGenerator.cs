c5ac61b0da64a7cf51e28047e403dd42459df879
nedas60 <nedas60@gmail.com>
2022-02-01 10:05:14 +0200

    Changed noise to Simplex, faster, works.

--- Voxel engine/World/Generation/ChunkGenerator.cs
+++ Voxel engine/World/Generation/ChunkGenerator.cs
@@ -9,13 +9,14 @@ namespace Voxel_engine.World.Generation
 {
     internal static class ChunkGenerator
     {
+        static Random rand = new Random();
         static int octaves = 3;
         public static Chunk GenerateChunk(int chunkx, int chunky)
         {
             //var data = Noise2d.GenerateChunkNoiseMap(chunkx, chunky, octaves);
             int poolId = ChunkArrayPool.Rent3DArray();
             float min = 0, max = 1;
-            var map = Perlin3D.GenerateChunkNoiseMap(chunkx, chunky, 1);
+            var map = Noise3D.GenerateChunkNoiseMap(chunkx, chunky, 1);
             byte[,,] blockTypes = ChunkArrayPool.arrayPool[poolId];
             for (int x = 0; x < 16; x++)
             {
@@ -26,15 +27,15 @@ namespace Voxel_engine.World.Generation
                         //float val = Perlin3D.Noise(x + chunkx*16, y, z + chunky * 16, 4, ref min, ref max);
                         //if (val > 0.01f) Console.WriteLine("Biggah");
                         //int height = (int)Math.Ceiling((1.0f + data[x * 16 + z]) / 2 * 255);
-                        blockTypes[x, y, z] = (map[x + z * 16 + y*16*16] > 0.5f ? (byte)1 : (byte)0);
+                        blockTypes[x, y, z] = (map[x * 16 * 256 + y * 16 + z] > 0.5 ? (byte)rand.Next(1,7) : (byte)0);
                     }
                 }
                 
             }
-            Console.WriteLine(min);
-            Console.WriteLine(max);
+            //Console.WriteLine(min);
+            //Console.WriteLine(max);
             //ArrayPool<float>.Shared.Return(data, true);
-            ArrayPool<float>.Shared.Return(map, true);
+            ArrayPool<double>.Shared.Return(map, true);
             return new Chunk(chunkx, chunky, blockTypes, poolId);
         }
 
